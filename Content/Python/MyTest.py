f7d6dc5c4f9b4b898fc0cbf9854e7d4a1cc3d6e1
UeharaYuto <y.uehara@vr-cnt.com>
2025-04-15 11:31:05 +0900

    ファイルの整理

--- Content/Python/MyTest.py
+++ Content/Python/MyTest.py
@@ -5,8 +5,8 @@ import sys
 actors = unreal.EditorActorSubsystem().get_selected_level_actors()
 print (actors)
 for actor in actors:
-
+        # 座標の取得
         location = unreal.Vector(float(sys.argv[1]),float(sys.argv[2]),float(sys.argv[3]))
 
-        # 例：位置変更
+        # 位置変更
         actor.set_actor_location(location,False,True)
